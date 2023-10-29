c66788a500273df691931c5318bb5c6cf394c174
Anant Negi <54684361+codeeshop-oc@users.noreply.github.com>
2023-10-29 12:38:12 +0530

    Update ces_module_backup.php

--- ces_module_backup.php
+++ ces_module_backup.php
@@ -1,17 +1,25 @@
 <?php
 	if(empty($_GET['er'])) {
 		error_reporting(0);
-	} else {
+	} else {		
 		ini_set('display_errors', 1);
 		error_reporting(E_ALL);
 	}
-	class Copy {
 
-		function recursive_copy($src, $dst) {
+	class BackupFiles {
+		public static $json = [];
+		public static $current_path = '';
+
+		public function updateJson($json_data, $current_path) {
+			self::$json = $json_data;
+			self::$current_path = $current_path;
+		}
+
+		public function recursive_copy($src, $dst) {
 			if(is_dir($src)) {
 				$dir = opendir($src);
 
-				mkdir($dst, 0777, true);
+				@mkdir($dst, 0777, true);
 
 				while(( $file = readdir($dir)) ) {
 					if (( substr($file, -1) != '_' ) && ( $file != '.' ) && ( $file != '..' )) {
@@ -19,7 +27,7 @@
 							$this->recursive_copy($src .'/'. $file, $dst .'/'. $file);
 						}
 						else {
-							copy($src .'/'. $file, $dst .'/'. $file);
+							$this->default_copy($src .'/'. $file, $dst .'/'. $file);
 						}
 					}
 				}
@@ -29,19 +37,69 @@
 			} elseif(is_file($src)) {
 				$dir = substr($dst, 0, strrpos($dst, '/'));
 				mkdir($dir, 0777, true);
-				copy($src, $dst);
+				$this->default_copy($src, $dst);
 			} else {
 				die('<strong>Not found : </strong> ' . $src);
 			}
 		}
+
+		public function default_copy($source, $destination) {
+			$all_files_ignore = self::$json['all_files_ignore'];
+
+			$new_current_file = str_replace(self::$current_path, '', $source);			
+			if(!in_array($new_current_file, $all_files_ignore)) {
+				copy($source, $destination);
+			}
+		}
+
+		public function move_to_folder($uploadFolder, $zipFileName) {
+	 		if (rename($zipFileName, $uploadFolder . '/' . $zipFileName)) {
+		        echo "ZIP file moved to the 'upload' folder.";
+		    } else {
+		        echo "Failed to move the ZIP file to the 'upload' folder.";
+		    }
+			echo "<br>";
+		}
+
+		public function zipFiles($folderToZip, $zipFileName, $current_path) {
+			// Create a new ZipArchive object
+			$zip = new ZipArchive();
+
+			// Open the ZIP file for writing
+			if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
+			    // Add files and subdirectories to the ZIP file
+			    $files = new RecursiveIteratorIterator(
+			        new RecursiveDirectoryIterator($folderToZip),
+			        RecursiveIteratorIterator::SELF_FIRST
+			    );
+			    
+		        foreach ($files as $file) {
+			        $file = realpath($file);
+			        $localName = str_replace($current_path, '', $file);
+			        if (is_dir($file)) {
+			            // Do not add directories
+			            continue;
+			        } elseif (is_file($file)) {
+			            $zip->addFile($file, $localName);
+			        }
+			    }
+
+			    // Close the ZIP file
+			    $zip->close();
+
+			    echo "ZIP archive created successfully!";
+			} else {
+			    echo "Failed to create ZIP archive.";
+			}
+			echo "<br>";
+		}
 	}
 
-	$copy = new Copy();
+	$backupFiles = new BackupFiles();
 	$done = '';
 	$current_path = getcwd() . '/';
 
 	if(isset($_POST['submit']) && $_POST['submit'] == 'Submit') {
-		// print_r(getcwd());
 		try {
 			$json = file_get_contents($_FILES['file']['tmp_name']);
 		} catch(Exception $e) {
@@ -51,6 +109,7 @@
 
 		$json = json_decode($json, true);
 
+		$backupFiles->updateJson($json, $current_path);
 
 		$folder_name = $json['default_copy_path'];
 		$fix_dst = '';
@@ -73,66 +132,23 @@
 
 		$current_new_path = str_replace("//", "/", $current_path . $json['default_path'] . '/');
 		foreach ($json['all_files'] as $key => $folder) {
-			$copy->recursive_copy($current_new_path . $folder, $fix_dst . $folder);
+			$backupFiles->recursive_copy($current_new_path . $folder, $fix_dst . $folder);
 		}
 		foreach ($json['single_file'] as $key => $file) {
-			$copy->recursive_copy($current_new_path . $file, $fix_dst . $file);
+			$backupFiles->recursive_copy($current_new_path . $file, $fix_dst . $file);
 		}
 
 		// echo $fix_dst;
 		// die;
 		echo "Zip Files<br>";
 		$zipFileName = 'oc_module.zip';
-		zipFiles($fix_dst, $zipFileName, $current_path);
-		move_to_folder($fix_dst, $zipFileName);
+		$backupFiles->zipFiles($fix_dst, $zipFileName, $current_path);
+		$backupFiles->move_to_folder($fix_dst, $zipFileName);
 
 		umask($old_umask);
 		$done = 'success';
 	}
-
-	function move_to_folder($uploadFolder, $zipFileName) {
- 		if (rename($zipFileName, $uploadFolder . '/' . $zipFileName)) {
-	        echo "ZIP file moved to the $uploadFolder folder.";
-	    } else {
-	        echo "Failed to move the ZIP file to the $uploadFolder folder.";
-	    }
-		echo "<br>";
-	}
-
-	function zipFiles($folderToZip, $zipFileName, $current_path) {
-		// Define the folder you want to zip
-		
-		// Create a new ZipArchive object
-		$zip = new ZipArchive();
-
-		// Open the ZIP file for writing
-		if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
-		    // Add files and subdirectories to the ZIP file
-		    $files = new RecursiveIteratorIterator(
-		        new RecursiveDirectoryIterator($folderToZip),
-		        RecursiveIteratorIterator::SELF_FIRST
-		    );
-		    
-	        foreach ($files as $file) {
-		        $file = realpath($file);
-		        $localName = str_replace($current_path, '', $file);
-		        if (is_dir($file)) {
-		            // Do not add directories
-		            continue;
-		        } elseif (is_file($file)) {
-		            $zip->addFile($file, $localName);
-		        }
-		    }
-
-		    // Close the ZIP file
-		    $zip->close();
-
-		    echo "ZIP archive created successfully!";
-		} else {
-		    echo "Failed to create ZIP archive.";
-		}
-		echo "<br>";
-	}
+ 
 ?>
 
 <!DOCTYPE html>
